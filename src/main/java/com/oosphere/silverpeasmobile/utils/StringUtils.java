305b2c84f149f7eb7259d3c099d25ececd8631c0
Sebastien Vuillet <sebastien.vuillet@silverpeas.com>
2011-10-25 12:54:48 +0000

    raz
    git-svn-id: https://www.silverpeas.org/svn/websites/silverpeasmobile/trunk@3193 d8afafa6-9ac6-4c14-b6a9-d96ce51c8d8f

--- src/main/java/com/oosphere/silverpeasmobile/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.oosphere.silverpeasmobile.utils;
-
-public class StringUtils {
-
-  public static boolean isValued(String value) {
-    return (value != null && value.length() > 0);
-  }
-  
-}
