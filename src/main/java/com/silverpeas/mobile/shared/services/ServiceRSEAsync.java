3d8aa8fcb26474fef848ba7b76eeb1f88449b940
Edouard Thomas <edouard.thomas@silverpeas.com>
2011-10-27 09:26:20 +0000

    git-svn-id: https://www.silverpeas.org/svn/websites/silverpeasmobile/trunk@3214 d8afafa6-9ac6-4c14-b6a9-d96ce51c8d8f

--- src/main/java/com/silverpeas/mobile/shared/services/ServiceRSEAsync.java
+++
@@ -1,9 +0,0 @@
-package com.silverpeas.mobile.shared.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ServiceRSEAsync {
-
-	void updateStatus(String status, AsyncCallback<Void> callback);
-
-}
