7ee90d43406551543138e5ecf848c40ead662cab
Edouard Thomas <edouard.thomas@silverpeas.com>
2011-11-04 13:52:20 +0000

    Upload du projet Android sur SVN.
    git-svn-id: https://www.silverpeas.org/svn/websites/silverpeasmobile/trunk@3260 d8afafa6-9ac6-4c14-b6a9-d96ce51c8d8f

---
+++ android/src/com/silverpeas/mobile/android/SilverpeasMobileAndroidActivity.java
@@ -0,0 +1,58 @@
+package com.silverpeas.mobile.android;
+
+import com.phonegap.*;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SilverpeasMobileAndroidActivity extends DroidGap {
+	
+	public static final String PREFS_NAME = "URLPrefsFile";
+	
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.main);
+        
+        final SharedPreferences URLPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String prefsURL = URLPrefs.getString("URL", "null");
+        
+        if(prefsURL=="null"){
+        	LayoutInflater factory = LayoutInflater.from(this);
+            final View alertDialogView = factory.inflate(R.layout.alertdialogurl, null);
+            
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            
+            alertDialog.setView(alertDialogView);
+            alertDialog.setTitle("Entrez l'URL du serveur : ");
+            alertDialog.setIcon(android.R.drawable.ic_dialog_alert);
+            
+            alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    				SharedPreferences.Editor prefsEditor = URLPrefs.edit();
+    				EditText EditTextURL = (EditText)alertDialogView.findViewById(R.id.EditTextURL);
+    				prefsEditor.putString("URL", EditTextURL.getText().toString());
+    				prefsEditor.commit();
+    				
+    				String URLenr = new String("URL Enregistrée.");	
+    				Toast.makeText(SilverpeasMobileAndroidActivity.this, URLenr, Toast.LENGTH_SHORT).show();
+    			}
+    		});
+            
+            alertDialog.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {		
+    			public void onClick(DialogInterface dialog, int which) {
+    				finish();
+    			}
+    		});
+            alertDialog.show();
+        }
+        else{
+            super.loadUrl("file:///android_asset/www/index.html");
+        }
+    }
+}
