bf8648078613d9074444baf8c24ee202520d24ce
Sebastien Vuillet <sebastien.vuillet@silverpeas.com>
2012-02-08 21:06:32 +0000

    Suppression ancienne gallery
    git-svn-id: https://www.silverpeas.org/svn/websites/silverpeasmobile/trunk@4063 d8afafa6-9ac6-4c14-b6a9-d96ce51c8d8f

--- web/src/main/java/com/silverpeas/mobile/client/pages/gallery/controler/event/GalleryEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.silverpeas.mobile.client.pages.gallery.controler.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface GalleryEventHandler extends EventHandler {
-	void onLoadedSettings(GalleryLoadedSettingsEvent event);
-}
