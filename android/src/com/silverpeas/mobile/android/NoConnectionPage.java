954b77376075f045e7c768a4b3f7440c4dc64f53
Edouard THOMAS <edouard.thomas@silverpeas.com>
2012-07-02 11:38:39 +0200

    ignore bin

--- android/src/com/silverpeas/mobile/android/NoConnectionPage.java
+++ android/src/com/silverpeas/mobile/android/NoConnectionPage.java
@@ -1,11 +1,29 @@
 package com.silverpeas.mobile.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NoConnectionPage extends Activity{
-	public void OnCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.noconnection);
+	}
+	
+	public void Quitter(View view){
+		finish();
+	}
+	
+	public void Informations(View view){
+		Intent intent = new Intent(NoConnectionPage.this, InformationsPage.class);
+		startActivityForResult(intent, 0);
+	}
+	
+	public void onBackPressed() {
+	    super.onBackPressed();   
+	    Intent intent = new Intent(NoConnectionPage.this, SilverpeasMobileActivity.class);
+		startActivityForResult(intent, 0);
+		finish();
 	}
 }
