524c0b3b3fed89b7b039142eb003dba9a8ffe9e2
sebastien vuillet <sebastien.vuillet@silverpeas.com>
2013-01-16 19:54:28 +0100

    Correct double attachement download.

--- web/src/main/java/com/silverpeas/mobile/client/apps/documents/pages/widgets/AttachmentWidget.java
+++ web/src/main/java/com/silverpeas/mobile/client/apps/documents/pages/widgets/AttachmentWidget.java
@@ -2,64 +2,70 @@ package com.silverpeas.mobile.client.apps.documents.pages.widgets;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptException;
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.InlineHTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Widget;
+import com.gwtmobile.ui.client.widgets.HorizontalPanel;
+import com.gwtmobile.ui.client.widgets.VerticalPanel;
 import com.silverpeas.mobile.client.apps.documents.resources.DocumentsMessages;
 import com.silverpeas.mobile.client.apps.documents.resources.DocumentsResources;
 import com.silverpeas.mobile.client.common.Notification;
 import com.silverpeas.mobile.client.common.mobil.MobilUtils;
 import com.silverpeas.mobile.client.common.phonegap.ChildBrowser;
-import com.silverpeas.mobile.client.common.phonegap.Downloader;
 import com.silverpeas.mobile.client.resources.ApplicationMessages;
 import com.silverpeas.mobile.shared.dto.documents.AttachmentDTO;
 
-public class AttachmentWidget extends Composite {
+public class AttachmentWidget extends Composite implements ClickHandler {
 
-	private static AttachmentWidgetUiBinder uiBinder = GWT.create(AttachmentWidgetUiBinder.class);
 	private AttachmentDTO attachement;
 	
-	@UiField(provided = true) protected DocumentsMessages msg = null;
-	@UiField(provided = true) protected ApplicationMessages globalMsg = null;
-	@UiField(provided = true) protected DocumentsResources ressources = null;
+	protected DocumentsMessages msg = null;
+	protected ApplicationMessages globalMsg = null;
+	protected DocumentsResources ressources = null;
 	
-	@UiField Label title, size, date, icon;
+	private HorizontalPanel grid = new HorizontalPanel();
+	private VerticalPanel internalGrid = new VerticalPanel();
+	private HorizontalPanel internalRow = new HorizontalPanel();	
+	private Label icon = new Label();
+	private Label title = new Label();
+	private Label size = new Label();
+	private Label date = new Label();
+	
+	private boolean clicked = false;
 
-	
-	
-	interface AttachmentWidgetUiBinder extends UiBinder<Widget, AttachmentWidget> {
-	}
-	
 	public AttachmentWidget() {
+		
 		ressources = GWT.create(DocumentsResources.class);		
 		ressources.css().ensureInjected();
 		msg = GWT.create(DocumentsMessages.class);
 		globalMsg = GWT.create(ApplicationMessages.class);
-		initWidget(uiBinder.createAndBindUi(this));
-	}
-	
-	@UiHandler("item")
-	void download(ClickEvent event) {	
-		try {
-			String url = Window.Location.getProtocol() + "//" + Window.Location.getHost() + attachement.getUrl();			
-			if (MobilUtils.isMobil()) {
-				ChildBrowser.openExternal(url, false);
-			} else {				
-				Window.open(url, "_blank", "");
-			}			
-			//TODO : use Downloader
-			//Downloader.downloadFile(url);
-		} catch(JavaScriptException e) {
-			Notification.alert(e.getMessage(), null, "error", "ok");
-		}
+		
+		grid.add(icon);		
+		grid.add(internalGrid);
+		grid.addDomHandler(this, ClickEvent.getType());	
+		internalGrid.add(title);
+		internalGrid.add(internalRow);
+		internalRow.add(size);
+		internalRow.add(new InlineHTML("&nbsp;-&nbsp;"));
+		internalRow.add(date);
+		initWidget(grid);
 	}
 
+	public String getTitle() {
+		return title.getText();
+	}
+
+	public void setTitle(String text) {
+		title.setText(text);
+	}
+
+	
+	
 	public void setAttachment(AttachmentDTO attachmentDTO) {
 		this.attachement = attachmentDTO;
 		render();
@@ -85,5 +91,34 @@ public class AttachmentWidget extends Composite {
 			icon.setStylePrimaryName(ressources.css().unknown());
 		}
 		
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {		
+		if (!clicked) {
+			clicked = true;
+			clickAction();
+			Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
+				@Override
+				public boolean execute() {
+					clicked = false;
+					return false;
+				}}, 300);
+		}		
+	}
+
+	private void clickAction() {
+		try {
+			String url = Window.Location.getProtocol() + "//" + Window.Location.getHost() + attachement.getUrl();			
+			if (MobilUtils.isMobil()) {
+				ChildBrowser.openExternal(url, false);				
+			} else {				
+				Window.open(url, "_blank", "");
+			}			
+			//TODO : use Downloader
+			//Downloader.downloadFile(url);
+		} catch(JavaScriptException e) {
+			Notification.alert(e.getMessage(), null, "error", "ok");
+		}
 	}
 }
