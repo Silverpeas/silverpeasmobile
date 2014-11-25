15c0afe456234d4605499eb074be1b90a9e29efa
svuillet <svu@silverpeas.com>
2014-11-25 20:44:50 +0100

    correct search result

--- web/src/main/java/com/silverpeas/mobile/server/services/ServiceSearchImpl.java
+++ web/src/main/java/com/silverpeas/mobile/server/services/ServiceSearchImpl.java
@@ -48,20 +48,24 @@ public class ServiceSearchImpl extends AbstractAuthenticateService implements Se
       for (MatchingIndexEntry result : r.getEntries()) {
 
         if (result.getObjectType().equals(ContentsTypes.Photo.toString()) || result.getObjectType().equals(ContentsTypes.Publication.toString()) || result.getObjectType().contains(ContentsTypes.Attachment.toString())) {
-          ResultDTO entry = new ResultDTO();
+          String title = result.getTitle(getUserInSession().getUserPreferences().getLanguage());
+          if (title != null && title.contains("wysiwyg") == false) {
+            ResultDTO entry = new ResultDTO();
 
-          if (result.getObjectType().contains(ContentsTypes.Attachment.toString())) {
-            entry.setType(ContentsTypes.Attachment.toString());
-            String attachmentId = result.getObjectType().replace("Attachment","");
-            attachmentId = attachmentId.replace("_" + getUserInSession().getUserPreferences().getLanguage(), "");
-            entry.setAttachmentId(attachmentId);
-          } else {
-            entry.setType(result.getObjectType());
+            if (result.getObjectType().contains(ContentsTypes.Attachment.toString())) {
+              entry.setType(ContentsTypes.Attachment.toString());
+              String attachmentId = result.getObjectType().replace("Attachment", "");
+              attachmentId = attachmentId
+                  .replace("_" + getUserInSession().getUserPreferences().getLanguage(), "");
+              entry.setAttachmentId(attachmentId);
+            } else {
+              entry.setType(result.getObjectType());
+            }
+            entry.setId(result.getObjectId());
+            entry.setTitle(title);
+            entry.setComponentId(result.getComponent());
+            results.add(entry);
           }
-          entry.setId(result.getObjectId());
-          entry.setTitle(result.getTitle(getUserInSession().getUserPreferences().getLanguage()));
-          entry.setComponentId(result.getComponent());
-          results.add(entry);
         }
       }
 
