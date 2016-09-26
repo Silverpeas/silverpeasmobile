702796433046d9971c7e5a72804be2a417f98521
svuillet <svu@silverpeas.com>
2016-09-26 15:26:51 +0200

    migration V6

--- bridge/mobile-war/src/main/java/com/silverpeas/bridge/MobilFilter.java
+++ bridge/mobile-war/src/main/java/com/silverpeas/bridge/MobilFilter.java
@@ -1,30 +1,20 @@
 package com.silverpeas.bridge;
 
-import com.silverpeas.gallery.control.ejb.GalleryBm;
-import com.silverpeas.gallery.model.Media;
-import com.silverpeas.gallery.model.MediaPK;
-import com.stratelia.silverpeas.silvertrace.SilverTrace;
-import com.stratelia.webactiv.util.EJBUtilitaire;
-import com.stratelia.webactiv.util.JNDINames;
-import com.stratelia.webactiv.util.publication.control.PublicationBm;
-import com.stratelia.webactiv.util.publication.model.PublicationDetail;
-import com.stratelia.webactiv.util.publication.model.PublicationPK;
+import org.silverpeas.components.gallery.model.Media;
+import org.silverpeas.components.gallery.model.MediaPK;
+import org.silverpeas.components.gallery.service.GalleryService;
+import org.silverpeas.components.gallery.service.MediaServiceProvider;
+import org.silverpeas.core.contribution.publication.model.PublicationDetail;
+import org.silverpeas.core.contribution.publication.model.PublicationPK;
+import org.silverpeas.core.contribution.publication.service.PublicationService;
 
-import java.io.IOException;
-
+import javax.servlet.*;
 import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MobilFilter implements Filter {
-
-  private PublicationBm pubBm;
-  private GalleryBm galleryBm;
 
   @Override
   public void destroy() {
@@ -55,7 +45,7 @@ public class MobilFilter implements Filter {
               appId;
         } else if (url.contains("Media")) {
           String id = url.substring(url.lastIndexOf("/") + 1);
-          Media media = getGalleryBm().getMedia(new MediaPK(id));
+          Media media = getGalleryService().getMedia(new MediaPK(id));
           String appId = media.getInstanceId();
           params = "?shortcutContentType=Media&shortcutContentId=" + id + "&shortcutAppId=" + appId;
         } else {
@@ -78,26 +68,12 @@ public class MobilFilter implements Filter {
   public void init(FilterConfig arg0) throws ServletException {
   }
 
-  private PublicationBm getPubBm() {
-    if (pubBm == null) {
-      try {
-        pubBm = EJBUtilitaire.getEJBObjectRef(JNDINames.PUBLICATIONBM_EJBHOME, PublicationBm.class);
-      } catch(Exception e) {
-        SilverTrace.error("MobilFilter", "MobilFilter.getPubBm", "root.EX_NO_MESSAGE", e);
-      }
-    }
-    return pubBm;
+  private PublicationService getPubBm() {
+    return PublicationService.get();
   }
 
-  private GalleryBm getGalleryBm() {
-    if (galleryBm == null) {
-      try {
-        galleryBm = EJBUtilitaire.getEJBObjectRef(JNDINames.GALLERYBM_EJBHOME, GalleryBm.class);
-      } catch (Exception e) {
-        SilverTrace.error("MobilFilter", "MobilFilter.getGalleryBm", "root.EX_NO_MESSAGE", e);
-      }
-    }
-    return galleryBm;
+  private GalleryService getGalleryService() {
+    return MediaServiceProvider.getMediaService();
   }
 
 }
