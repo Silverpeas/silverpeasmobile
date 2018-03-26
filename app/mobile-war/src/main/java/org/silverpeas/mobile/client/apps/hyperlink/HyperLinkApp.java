68e83201b04e97a2fbbcbc91210a67858e49d805
svuillet <svu@silverpeas.com>
2018-03-26 12:44:20 +0200

    feature https://tracker.silverpeas.org/issues/9142

---
+++ app/mobile-war/src/main/java/org/silverpeas/mobile/client/apps/hyperlink/HyperLinkApp.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2000 - 2017 Silverpeas
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.silverpeas.mobile.client.apps.hyperlink;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.silverpeas.mobile.client.apps.blog.events.app.AbstractBlogAppEvent;
+import org.silverpeas.mobile.client.apps.blog.events.app.BlogAppEventHandler;
+import org.silverpeas.mobile.client.apps.blog.events.app.BlogLoadEvent;
+import org.silverpeas.mobile.client.apps.blog.events.pages.BlogLoadedEvent;
+import org.silverpeas.mobile.client.apps.blog.pages.BlogPage;
+import org.silverpeas.mobile.client.apps.blog.resources.BlogMessages;
+import org.silverpeas.mobile.client.apps.comments.events.app.CommentsLoadEvent;
+import org.silverpeas.mobile.client.apps.comments.events.pages.CommentsLoadedEvent;
+import org.silverpeas.mobile.client.apps.navigation.events.app.external.AbstractNavigationEvent;
+import org.silverpeas.mobile.client.apps.navigation.events.app.external.NavigationAppInstanceChangedEvent;
+import org.silverpeas.mobile.client.apps.navigation.events.app.external.NavigationEventHandler;
+import org.silverpeas.mobile.client.apps.navigation.events.app.external.NavigationShowContentEvent;
+import org.silverpeas.mobile.client.common.EventBus;
+import org.silverpeas.mobile.client.common.Notification;
+import org.silverpeas.mobile.client.common.ServicesLocator;
+import org.silverpeas.mobile.client.common.app.App;
+import org.silverpeas.mobile.client.common.network.AsyncCallbackOnlineOrOffline;
+import org.silverpeas.mobile.client.common.storage.LocalStorageHelper;
+import org.silverpeas.mobile.shared.dto.blog.PostDTO;
+import org.silverpeas.mobile.shared.dto.comments.CommentDTO;
+import org.silverpeas.mobile.shared.dto.navigation.ApplicationInstanceDTO;
+import org.silverpeas.mobile.shared.dto.navigation.Apps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HyperLinkApp extends App implements NavigationEventHandler {
+
+  private ApplicationInstanceDTO instance;
+
+  public HyperLinkApp(){
+    super();
+    EventBus.getInstance().addHandler(AbstractNavigationEvent.TYPE, this);
+  }
+
+  public void start(){
+    // always start
+  }
+
+  @Override
+  public void stop() {
+    // nevers stop
+  }
+
+
+  @Override
+  public void appInstanceChanged(final NavigationAppInstanceChangedEvent event) {
+    if (event.getInstance().getType().equals(Apps.hyperlink.name())) {
+      this.instance = event.getInstance();
+      final String key = "hyperlink" + "_" + event.getInstance().getId();
+      AsyncCallbackOnlineOrOffline action = new AsyncCallbackOnlineOrOffline<String>(getOfflineAction(event, key)) {
+        @Override
+        public void onSuccess(String result) {
+          super.onSuccess(result);
+          LocalStorageHelper.store(key, String.class, result);
+          openLink(result);
+        }
+
+        @Override
+        public void attempt() {
+          ServicesLocator.getServiceHyperLink().getUrl(instance.getId(), this);
+        }
+      };
+      action.attempt();
+    }
+  }
+
+  private void openLink(String url) {
+    Notification.activityStop();
+    Window.open(url,"_blank","");
+  }
+
+  private Command getOfflineAction(final NavigationAppInstanceChangedEvent event, final String key) {
+    Command offlineAction = new Command() {
+
+      @Override
+      public void execute() {
+        String result = LocalStorageHelper.load(key, String.class);
+        if (result == null) {
+          result = "";
+        }
+        openLink(result);
+      }
+    };
+
+    return offlineAction;
+  }
+
+  @Override
+  public void showContent(final NavigationShowContentEvent event) {
+  }
+}
