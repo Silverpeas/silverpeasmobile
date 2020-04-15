f0f4563483e676f76b68a902f857847bdb289ea3
svuillet <svu@silverpeas.com>
2020-04-15 10:25:37 +0200

    fix msql script

--- mobile-configuration/src/main/config/migrations/db/mssql/spmobile/001/create_table.sql
+++ mobile-configuration/src/main/config/migrations/db/mssql/spmobile/001/create_table.sql
@@ -1,13 +1,4 @@
 
-
-CREATE TABLE SC_Gallery_OrderDetail (
-  orderId          VARCHAR(40) NOT NULL,
-  mediaId          VARCHAR(40) NOT NULL,
-  instanceId       VARCHAR(50) NOT NULL,
-  downloadDate     datetime   NULL,
-  downloadDecision VARCHAR(50) NULL
-);
-
 CREATE TABLE st_token_pushnotification
 (
   id bigint NOT NULL,
