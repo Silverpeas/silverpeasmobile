f992b5d017f07488afb98e43c067d90aaec8bed8
svuillet <svu@silverpeas.com>
2021-06-11 18:10:14 +0200

    Bug #12392

--- mobile-war/src/main/java/org/silverpeas/mobile/client/components/forms/FieldEditable.java
+++ mobile-war/src/main/java/org/silverpeas/mobile/client/components/forms/FieldEditable.java
@@ -113,7 +113,7 @@ public class FieldEditable extends Composite implements ChangeHandler, ValueChan
       String value = "";
       ListBox l = ((ListBox) w);
       l.clear();
-      l.setVisibleItemCount(event.getUsersAndGroupsSelected().size() + 1);
+      l.setVisibleItemCount(event.getUsersAndGroupsSelected().size());
       for (BaseDTO dto : event.getUsersAndGroupsSelected()) {
         if (dto instanceof UserDTO) {
           UserDTO u = (UserDTO) dto;
@@ -272,7 +272,6 @@ public class FieldEditable extends Composite implements ChangeHandler, ValueChan
       }
       l.setWidth("10em");
       l.setEnabled(!data.isReadOnly());
-      l.setVisibleItemCount(2);
       l.addClickHandler(new ClickHandler() {
         @Override
         public void onClick(final ClickEvent clickEvent) {
