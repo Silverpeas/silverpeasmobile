648f64c69848291efd5c05d8534999e36f61c53e
SilverYoCha <yohann.chastagnier@silverpeas.com>
2021-10-05 15:59:55 +0200

    Feature #12584: applying the new project architecture around core-rs and core-web.
    Updating old copyrights.

--- mobile-war/src/main/java/org/silverpeas/mobile/server/services/ServiceNews.java
+++ mobile-war/src/main/java/org/silverpeas/mobile/server/services/ServiceNews.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2000 - 2021 Silverpeas
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -10,7 +11,7 @@
  * Open Source Software ("FLOSS") applications as described in Silverpeas's
  * FLOSS exception.  You should have received a copy of the text describing
  * the FLOSS exception, and it is also available here:
- * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
+ * "https://www.silverpeas.org/legal/floss_exception.html"
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -26,8 +27,8 @@ package org.silverpeas.mobile.server.services;
 import org.silverpeas.components.quickinfo.model.News;
 import org.silverpeas.core.admin.service.OrganizationController;
 import org.silverpeas.core.annotation.WebService;
-import org.silverpeas.core.webapi.base.RESTWebService;
-import org.silverpeas.core.webapi.base.annotation.Authorized;
+import org.silverpeas.core.web.rs.RESTWebService;
+import org.silverpeas.core.web.rs.annotation.Authorized;
 import org.silverpeas.mobile.server.services.helpers.NewsHelper;
 import org.silverpeas.mobile.shared.dto.news.NewsDTO;
 
@@ -41,28 +42,6 @@ import javax.ws.rs.core.MediaType;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-/*
- * Copyright (C) 2000 - 2021 Silverpeas
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 @WebService
 @Authorized
 @Path(ServiceNews.PATH + "/{appId}")
