2f91baf3a4400f619a3d38ec76386a00521ca73f
svuillet <svu@silvepeas.com>
2023-09-15 15:20:17 +0200

    Fix #13747

--- mobile-war/src/main/java/org/silverpeas/mobile/client/resources/ApplicationMessages.java
+++ mobile-war/src/main/java/org/silverpeas/mobile/client/resources/ApplicationMessages.java
@@ -45,7 +45,7 @@ public interface ApplicationMessages extends Messages {
   @DefaultMessage("Corriger")
   String correctBtnLabel();
 
-  @DefaultMessage("© 2001-2020 <a target='_blank' href='http://www.silverpeas.com'>Silverpeas</a>	- Tous droits réservés")
+  @DefaultMessage("© 2001-2023 <a target='_blank' href='http://www.silverpeas.com'>Silverpeas</a>	- Tous droits réservés")
   SafeHtml copyright();
 
   @DefaultMessage("Parcourir la plateforme")
