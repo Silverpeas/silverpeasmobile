34a933dd7d54c62498c04c741c185e26c92f7497
svuillet <svu@silvepeas.com>
2023-11-14 16:23:23 +0100

    Fix #13849

---
+++ mobile-war/src/main/java/org/silverpeas/mobile/shared/dto/PropertyDTO.java
@@ -0,0 +1,24 @@
+package org.silverpeas.mobile.shared.dto;
+
+import java.io.Serializable;
+
+public class PropertyDTO implements Serializable {
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
